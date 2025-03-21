d7e31a23cd54f903e732169d5007eb426365e16a
Shawn Allen <Shawn.Allen@vu.com>
2025-03-21 13:20:12 -0500

    Separated code into reusable functions. Added number of client variable to loop through instead of having the code multiple times.

--- compoundInterest.py
+++ compoundInterest.py
@@ -1,12 +1,36 @@
-def calculateCompoundInterest():
-    
-# This first 3 lines are provided for yougetACompoundInterest()
-# This first 3 lines are provided for you
- client_one_principal = float(input("Principle (amount): "))
- client_one_time =      float(input("Time:               "))
- client_one_rate =      float(input("Rate:               "))
- #print("Compound Interest: "+str(intrest))
+def collect_user_input():
+    # User input
+    principal = float(input("Principle (amount): "))
+    time =      float(input("Time:               "))
+    rate =      float(input("Rate:               "))
 
+    return principal, time, rate
+
+def calculate_and_print_compound_interest(principal, time, rate):
+# Formula for amount
+    amount = principal * ( 1 + rate / 100) ** time
+
+    # Formula for compound interest
+    compound_interest = round(amount - principal, 2) # round to nearest hundredth
+
+    # Print result
+    print(f"Compound Interest: {compound_interest}")
+
+def calculateCompoundInterest():
+    number_of_clients = 3 # Declares number of clients for how many times the loop will ask
+
+    # Will loop through below depending on how many clients there are
+    for i in range(number_of_clients):
+        # Prints divider
+        if i != 0:
+            print("---")
+
+        # User input
+        principal, time, rate = collect_user_input()
+        
+        # Calculate and print Compound Interest
+        calculate_and_print_compound_interest(principal, time, rate)
+        
     # end assignment
 
 ## If you want to test locally run > python compoundInterest.py
